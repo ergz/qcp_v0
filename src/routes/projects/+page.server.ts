5568f05db9a099c223e466ce83165c1f2bfe8501
ergz <erodriguez@hey.com>
2024-08-04 22:51:28 -0700

    got the db working with projects page

--- src/routes/projects/+page.server.ts
+++ src/routes/projects/+page.server.ts
@@ -3,13 +3,12 @@ import {getAllProjects } from "$lib/database"
 export async function load() {
     const allProjects = await getAllProjects();
     return {
-       projects: allProjects.body.map((proj) => ({
+       projects: allProjects.body.data.map((proj) => ({
             name: proj.name,
             client: proj.client_id,
             budget: proj.budget,
             start_date: proj.start_date,
-            end_date: proj.end_date
-            
+            end_date: proj.end_date,
         }))
-    }
+    };
 }
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import { projects } from '$lib/data';
+
 	export let data;
 	console.log(data);
 </script>
