743d6ecc91d3b5323d84801135f82dfb39fd7b1f
ergz <e.emanuel.rodriguez@gmail.com>
2024-08-08 22:55:52 -0700

    just some tweaks and buttons

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -37,13 +37,31 @@
 						<td>{project.start_date}</td>
 						<td>{project.end_date}</td>
 						<th>
-							<a href="/projects/{project.id}" class="btn btn-outline btn-success btn-xs">Details</a
+							<a href="/projects/{project.id}" class="btn btn-outline btn-success btn-sm">Details</a
 							>
 						</th>
 						<th>
 							<form method="POST">
 								<input name="project_name_to_delete" type="hidden" value={project.name} />
-								<button class="btn btn-outline btn-error btn-xs">DELETE</button>
+								<button class="btn btn-outline btn-error btn-sm">
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										width="24"
+										height="24"
+										viewBox="0 0 24 24"
+										fill="none"
+										stroke="currentColor"
+										stroke-width="2"
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										class="icon icon-tabler icons-tabler-outline icon-tabler-trash"
+										><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M4 7l16 0" /><path
+											d="M10 11l0 6"
+										/><path d="M14 11l0 6" /><path
+											d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12"
+										/><path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" /></svg
+									>
+								</button>
 							</form>
 						</th>
 					</tr>
--- src/routes/projects/[id]/+page.svelte
+++ src/routes/projects/[id]/+page.svelte
@@ -2,4 +2,23 @@
 	export let data;
 </script>
 
-<h1 class="text-3xl">{data.project.name}</h1>
+<div class="w-3/5 mx-auto text-center px-4">
+	<div class="relative mb-4">
+		<a
+			href="/projects"
+			class="btn btn-success btn-md absolute left-0 top-0 p-2"
+			aria-label="Go back"
+		>
+			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="w-6 h-6 fill-current">
+				<path
+					d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z"
+				/>
+			</svg>
+		</a>
+		<h1 class="text-3xl text-center">{data.project.name}</h1>
+	</div>
+	<p class="w-2/5 mx-auto text-center m-10">
+		Here is a description of this project, this section needs to be added to all of the projects, by
+		adding this as field to the database and required form element in the add new project page
+	</p>
+</div>
