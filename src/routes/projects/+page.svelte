3d44c6e7ae5149493d3b4cf51ae5075b4872da07
ergz <e.emanuel.rodriguez@gmail.com>
2024-08-10 00:27:24 -0700

    adds delete confirmation for project deletions

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,5 +1,26 @@
 <script lang="ts">
+	import { createEventDispatcher } from 'svelte';
 	export let data;
+	let showModal = false;
+	let formElement: HTMLFormElement | null = null;
+
+	const dispatch = createEventDispatcher();
+
+	function openModal(event: Event) {
+		event.preventDefault();
+		showModal = true;
+	}
+
+	function handleConfirm() {
+		showModal = false;
+		if (formElement) {
+			formElement.submit();
+		}
+	}
+
+	function handleCancel() {
+		showModal = false;
+	}
 </script>
 
 <div class="w-3/5 mx-auto text-center px-4">
@@ -41,7 +62,7 @@
 							>
 						</th>
 						<th>
-							<form method="POST">
+							<form method="POST" bind:this={formElement} on:submit|preventDefault={openModal}>
 								<input name="project_name_to_delete" type="hidden" value={project.name} />
 								<button class="btn btn-outline btn-error btn-sm">
 									<svg
@@ -73,3 +94,18 @@
 		<a href="new-project" class="btn btn-outline btn-success">Add project</a>
 	</div>
 </div>
+
+{#if showModal}
+	<div
+		class="fixed inset-0 bg-opacity-50 backdrop-blur-sm overflow-y-auto h-full w-full flex items-center justify-center"
+	>
+		<div class="bg-neutral p-5 rounded-lg shadow-xl">
+			<h2 class="text-xl font-bold mb-4">Confirm Deletion</h2>
+			<p class="mb-4">Are you sure you want to delete the project?</p>
+			<div class="flex justify-end">
+				<button on:click={handleCancel} class="btn btn-info m-2">Cancel</button>
+				<button on:click={handleConfirm} class="btn btn-error m-2">Confirm</button>
+			</div>
+		</div>
+	</div>
+{/if}
